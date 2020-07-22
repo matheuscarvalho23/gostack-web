711113a71fe9caa1c59dc35ffee454c0376c3f0a
Matheus <matheus.carvalho@m2n.com.br>
2020-07-21 23:59:34 -0300

    Errors layout

--- src/components/Input/style.ts
+++ src/components/Input/style.ts
@@ -1,18 +1,40 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+import Tooltip from '../Tooltip';
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  isErrored: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
   background: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
   padding: 16px;
   width: 100%;
+
+  border: 2px solid #232129;
   color: #666360;
+
   display: flex;
   align-items: center;
 
   & + div {
     margin-top: 8px;
   }
+
+  ${props => props.isErrored && css`
+    border-color: #c53030;
+  `}
+
+  ${props => props.isFocused && css`
+    color: #FF9000;
+    border-color: #FF9000;
+  `}
+
+  ${props => props.isFilled && css`
+    color: #FF9000;
+  `}
 
   input {
     flex: 1;
@@ -28,4 +50,23 @@ export const Container = styled.div`
   svg {
     margin-right: 16px;
   }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
+  }
+
 `;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import {FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
+
+import getValidationsErrors from '../../utils/getBalidationsErrors';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -7,29 +12,54 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './style';
 import logo from '../../assets/logo.svg';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
+const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
 
-    <Content>
-      <img src={logo} alt="GoBarber"/>
+  const handleSubmit = useCallback(async (data: object) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Name is required.'),
+        email: Yup.string().required('E-mail is required').email(),
+        password: Yup.string().min(6, 'At least 6 digits'),
+      });
 
-      <form>
-        <h1>Faça seu cadastro</h1>
+      await schema.validate(data, {
+        abortEarly: false,
+      });
 
-        <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
-        <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
-        <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
+    } catch (err) {
 
-        <Button type="submit">Cadastrar</Button>
-      </form>
+      const errors = getValidationsErrors(err);
 
-      <a href="ddd">
-        <FiArrowLeft/>
-        Voltar para login
-      </a>
-    </Content>
-  </Container>
-);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+        <img src={logo} alt="GoBarber"/>
+
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+
+          <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
+          <Input name="email" icon={FiMail} type="text" placeholder="E-mail" />
+          <Input name="password" icon={FiLock} type="password" placeholder="Senha"/>
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="ddd">
+          <FiArrowLeft/>
+          Voltar para login
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
